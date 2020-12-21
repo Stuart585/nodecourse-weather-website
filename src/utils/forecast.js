206bff2767854535b7f5bdb602699ee9f4964053
stuart barker <stuart.barker@uqconnect.edu.au>
2020-12-21 08:03:07 -0500

    add forecast data

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,12 @@ const forecast = (latitude, longitude, callback) => {
        }else if(body.error) {
             callback ('Unable to find weather data!', undefined)
        }else {
-            callback(undefined, body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degrees outside. It feels like " + body.current.feelslike)
+          
+            callback(
+                undefined,
+                body.current.weather_descriptions[0] +". It is currently " + body.current.temperature + " degrees outside. It feels like " + 
+                body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + "%."
+            )
        }
     })
 }
